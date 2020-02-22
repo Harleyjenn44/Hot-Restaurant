6d8b11bf9f6f45ffe1bf86f198144f0afc9490b1
Joe Huelsman <kcgooner91@gmail.com>
2020-02-22 13:55:25 -0600

    added fix to js file

--- index.js
+++ index.js
@@ -11,31 +11,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Reservations array
-var reservations = [
-    {
-        name: "",
-        phoneNumber: "",
-        email: "",
-        ID: ""
-    }
-];
-
+var reservations = [];
 var waitList = []
-var currentReservations = [];
 
+// pages endpoints - serve html
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./mainpage.html"));
 });
+
 app.get("/table", function (req, res) {
     res.sendFile(path.join(__dirname, "./table.html"));
 });
+
 app.get("/makereservation", function (req, res) {
     res.sendFile(path.join(__dirname, "./reservation.html"))
 })
 
 
+// api endpoints - serve or receive data
 
+// $.get("/api/reservations",  callback)
+app.get("/api/reservations", function (req, res) {
+    res.json(reservations)
+})
 
+// $.post("/api/reservations", data, callback)
+app.post("/api/reservations", function (req, res) {
+    
+    // if is more than five push to waitList 
+    reservations.push(req.body)
+    res.json(reservations)
+})
 
 // Start our server
 app.listen(PORT, function () {
